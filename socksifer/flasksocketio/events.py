789ae0337f5555a3f1794b4b4335c319209a3129
skylerknecht <skyler.knecht@outlook.com>
2023-08-25 16:38:59 -0400

    Removed threading for downstream results due to SSL not playing well with threads

--- socksifer/flasksocketio/events.py
+++ socksifer/flasksocketio/events.py
@@ -62,8 +62,4 @@ class Events:
 
     @json_event_handler
     def socks_downstream_results(self, results):
-        threading.Thread(
-            target=socks_server_manager.handle_socks_task_results,
-            args=('socks_downstream', results,),
-            daemon=True
-        ).start()
+        socks_server_manager.handle_socks_task_results('socks_downstream', results)

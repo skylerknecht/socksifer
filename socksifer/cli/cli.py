9ed70a9b8dd13f9638bfa4d30d32f39fce379206
skylerknecht <skyler.knecht@outlook.com>
2023-09-01 08:55:24 -0400

    Working tab completion

--- socksifer/cli/cli.py
+++ socksifer/cli/cli.py
@@ -1,9 +1,9 @@
 import readline
 import sys
-import threading
 
-from .commands import commands_manager
+from .commands import commands, commands_manager
 from socksifer.output import display
+from socksifer.socks import socks_server_manager
 
 
 class CLI:
@@ -13,6 +13,10 @@ class CLI:
         self.output = []
         self.PROMPT = prompt
         self.prompt = self.PROMPT
+        self.completer = Completer(commands)
+        readline.parse_and_bind('tab: complete')
+        readline.set_completer(self.completer.complete_option)
+        readline.set_completer_delims(" \t\n\"\\'`@$><=;|&{(")
 
     def listen_for_user_input(self):
         display('Welcome to the sockiest proxy around, type help or ? to get started')
@@ -40,3 +44,39 @@ class CLI:
             self.prompt = self.PROMPT
             return
         self.prompt = prompt if prompt else self.prompt
+
+class Completer:
+    # ToDo: we need to change option to command - nomenclature
+    def __init__(self, commands):
+        self.commands = commands
+
+    def complete_option(self, incomplete_option, state):
+            '''
+            Analyzes the length of current line buffer / incomplete_option and
+            determines the user(s) completion.
+
+            If the current line buffer is greater or equal to one and the current line
+            buffer ends with a trailing space then that indicates the user is attempting
+            to complete a multi-worded option. The length of the current line buffer,
+            when delimeted by a space, must be incremented by one to correctly search
+            for the next option.
+
+            Otherwise, generate a list of all current menu options and file names that
+            start with the current incomplete_option aka the last line in the buffer.
+
+            Parameters:
+                    incomplete_option (str()): The current incomplete option.
+                    state (int()): An integer so that when the funciton is called
+                                recursivley by readline it can gather all items
+                                within the current finished_option list.
+
+            Returns:
+                    finished_option (str): Whatever option the callee has not
+                                        gathered yet.
+            '''
+            current_line = readline.get_line_buffer()
+            current_line_list = current_line.split()
+            if len(current_line_list) >= 1 and current_line.endswith(' '):
+                current_line_list.append('')
+            finished_options = [command for command in self.commands | socks_server_manager.socks_servers.keys() if command.startswith(incomplete_option)]
+            return finished_options[state]
